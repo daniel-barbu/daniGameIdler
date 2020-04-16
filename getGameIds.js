258cc9ba4dc117206b6777a9dbcb231e8f8efce4
daniel-barbu <63318773+daniel-barbu@users.noreply.github.com>
2020-04-16 23:35:04 +0300

    Update getGameIds.js

--- getGameIds.js
+++ getGameIds.js
@@ -1,5 +1,5 @@
 var game_ids="";
 for (i=0; i<document.getElementsByClassName("btn_green_white_innerfade").length; i++) {
-	game_ids+=document.getElementsByClassName("btn_green_white_innerfade")[i].parentNode.parentNode.parentNode.parentNode.parentNode.childNodes[1].href.split("/gamecards/")[1].replace("/","")+"\n";
+	game_ids+=document.getElementsByClassName("btn_green_white_innerfade")[i].parentNode.parentNode.parentNode.parentNode.parentNode.childNodes[1].href.split("/gamecards/")[1].replace("/","")+"<br>";
 }
-console.log(game_ids);
+document.write(game_ids);

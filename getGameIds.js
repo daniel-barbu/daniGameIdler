8a548f9050a0d48f19834f8aaf2a4a42caddd6d6
daniel-barbu <63318773+daniel-barbu@users.noreply.github.com>
2020-04-27 06:08:27 +0300

    Update getGameIds.js

--- getGameIds.js
+++ getGameIds.js
@@ -1,7 +1,6 @@
-var game_ids="";
-var drops=0;
+var gameIds="", drops=0;
 for (i=0; i<document.getElementsByClassName("btn_green_white_innerfade").length; i++) {
-	game_ids+=document.getElementsByClassName("btn_green_white_innerfade")[i].parentNode.parentNode.parentNode.parentNode.parentNode.childNodes[1].href.split("/gamecards/")[1].replace("/","")+"<br>";
+	gameIds+=document.getElementsByClassName("btn_green_white_innerfade")[i].parentNode.parentNode.parentNode.parentNode.parentNode.childNodes[1].href.split("/gamecards/")[1].replace("/","")+"<br>";
 	drops+=Number(document.getElementsByClassName("btn_green_white_innerfade")[i].parentNode.parentNode.childNodes[3].childNodes[3].childNodes[1].textContent.split(" ")[0]);
 }
-document.write("DROPS: "+drops+", GAMES:"+document.getElementsByClassName("btn_green_white_innerfade").length+"<br><br>----GAME IDS----<br>"+game_ids);
+document.write("DROPS: "+drops+", GAMES:"+document.getElementsByClassName("btn_green_white_innerfade").length+"<br><br>----GAME IDS----<br>"+gameIds); document.body.style.fontFamily="Lucida Console, monospace";
